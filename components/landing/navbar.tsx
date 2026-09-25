6b5d4f6bcaf5d731d5231438835851b95cc87fc4
Dot010 <jonathan2500@outlook.pt>
2026-09-25 17:00:15 -0300

    fix: update navbar styles for improved visibility and layout

--- components/landing/navbar.tsx
+++ components/landing/navbar.tsx
@@ -4,9 +4,9 @@ import { Button } from "@/components/ui/buttons"
 
 const Navbar = () => {
   return (
-    <header className="mx-auto flex max-w-7xl items-center justify-between px-6 py-6">
+    <header className="mx-auto flex max-w-7xl items-center justify-between px-6 py-6 absolute top-0 left-0 right-0 z-50 w-full bg-transparent">
       <Link href="/" className="flex items-center gap-3">
-        <div className="relative h-12 w-40 bg-amber-950">
+        <div className="relative h-12 w-40 ">
           <Image
             src="/images/beanscene.png"
             alt="Bean Scene"
@@ -18,7 +18,7 @@ const Navbar = () => {
         </div>
       </Link>
 
-      <nav className="hidden items-center gap-8 text-sm font-serif text-amber-950 md:flex">
+      <nav className="hidden items-center gap-8 text-sm font-serif text-coffee-cream md:flex">
         <Link href={'#home'}>Home</Link>
         <Link href={'#menu'}>Menu</Link>
         <Link href={'#about'}>About Us</Link>
@@ -26,7 +26,8 @@ const Navbar = () => {
       </nav>
 
       <div className="flex items-center gap-5">
-        <Button
+        <Button 
+        
          variant='ghost'
           href='#signIn'
         >
